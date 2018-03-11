b6690fcb8471a4ba98b2bf4897ee8f909212b627
EC2 Default User <ec2-user@ip-172-31-34-228.ec2.internal>
2018-03-11 08:27:37 +0000

    サインインサインアウトの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => "registrations"
+  }
+  resources :users, only: [:show]
   root 'static_pages#home'
   get  '/map',     to: 'static_pages#map' 
   get  '/contact', to: 'static_pages#contact'
