cd71090292952b17dcdb3adef315df45cf163b57
EC2 Default User <ec2-user@ip-172-31-34-228.ec2.internal>
2018-04-05 04:39:38 +0000

    aboutページの修正

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
-    @micropost = current_user.microposts.build if signed_in?
+    if signed_in?
+      @micropost = current_user.microposts.build
+      @feed_items = current_user.feed.all
+    end
   end
 
   def map
